d3984145535aa9d11b91bc91017d2ce721c57c7c
csan60 <csan60248@gmail.com>
2025-10-28 17:41:34 +0800

    Delete MonteCarlofor2048_1732926125305 directory

--- MonteCarlofor2048_1732926125305/src/main/java/net/mooctest/Measure.java
+++
@@ -1,7 +0,0 @@
-package net.mooctest;
-
-import net.mooctest.Board;
-
-public interface Measure {
-	double score(Board board);
-}
--- MonteCarlofor2048_1732926125305/src/main/java/net/mooctest/Strategy.java
+++
@@ -1,7 +0,0 @@
-package net.mooctest;
-
-import net.mooctest.Board;
-
-public interface Strategy {
-	public Board play(Board board);
-}
