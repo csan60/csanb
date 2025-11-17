81c9af6eb55204c2aad5eb7bd057adda0b69d4c1
csan60 <csan60248@gmail.com>
2025-11-17 14:06:09 +0800

    Delete 省赛 directory

--- 省赛/project/测试脚本/12_测试2.lua
+++
@@ -1,9 +0,0 @@
-local T ={7,8,9}
-
-function entry()
-  for _, value in ipairs(T) do
-    local res =write_msg(channels.Sensor,protocols.P_Sensor,{temprature =value})
-    etimer.delay(100)
-  end
-  exit()
-end
--- 省赛/project/测试脚本/13_测试3.lua
+++
@@ -1,8 +0,0 @@
-function entry()
-  local res =write_msg(channels.Sensor,protocols.P_Sensor,{temprature =11,datatype1=0x02})
-  etimer.delay(100)
-  passed = ask('yesno',  {title='提示', msg='观察界面采集温度是否为11', default=true})
-  local res =step("验证数据类型1=0x02","不显示采集到数据，软件做丢包处理","显示到采集的数据",not passed)
-  check(res,"数据类型1=0x02,测试通过!","数据类型1=0x02,测试不通过")
-  exit()
-end
